806db56bd128dd38c73e07c749c90bc0f39cbcbe
NitaiEdelberg <nitai.edel@gmail.com>
2025-04-26 21:00:14 +0300

    site completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 import productRoutes from "./routes/product.route.js";
 
@@ -9,9 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); // to get the current directory name
+
 app.use(express.json()); // allows to accept JSON data in the req.body
 
 app.use("/api/products",productRoutes);
+// Serve frontend files in production
+if(process.env.NODE_ENV.trim() === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+// if the request is for any route other than /api/products, serve the index.html file
+    app.get(/^\/(?!api).*$/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
