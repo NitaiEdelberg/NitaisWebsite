f0475161ff21bde708dbd4705be89725e3f6327e
NitaiEdelberg <nitai.edel@gmail.com>
2025-05-06 00:46:13 +0300

    route with middleware

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,12 +1,13 @@
 import express from 'express'; 
+import { authMiddleware } from '../middleware/auth.js';
 
 import { createMovie, deleteMovie, getMovies, updateMovie } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
-router.get("/", getMovies);
-router.post("/",createMovie);
-router.delete("/:id",deleteMovie);
-router.put("/:id",updateMovie);
+router.get("/", authMiddleware, getMovies);
+router.post("/", authMiddleware, createMovie);
+router.delete("/:id", authMiddleware, deleteMovie);
+router.put("/:id", authMiddleware, updateMovie);
 
 export default router;
